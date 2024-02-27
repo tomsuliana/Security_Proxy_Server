d9f7451b9cc8573521d10b594d12d67bfc387da3
tomsuliana <ultomskaya@yandex.ru>
2024-02-27 23:55:05 +0300

    Mongo connected

--- proxy-server/app/main.go
+++ proxy-server/app/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net"
 	"proxy-server/proxy-server/pkg/proxy"
+	"time"
 )
 
 const PROXYPORT = 8080
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	connectionString := fmt.Sprintf("mongodb://%s:%s@%s:%d", "root", "example", "mongo", 27017)
+	mongoConnection, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	proxyHandler, err := proxy.NewHandler()
 	if err != nil {
 		fmt.Println(err)
